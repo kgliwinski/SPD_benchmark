08bd663a0f912778106e1dabb1c9aa7df3637dd8
kgliwinski <259282@student.pwr.edu.pl>
2023-04-21 16:17:56 +0200

    Finished witi sriti

--- WiTi/main.cpp
+++ WiTi/main.cpp
@@ -4,9 +4,13 @@
 #include <stdio.h>
 #include <chrono>
 #include <iostream>
+#include <fstream>
+typedef std::vector<Job *> (*fun)(std::vector<Job *>);
 
-typedef std::vector<Job*> (*fun)(std::vector<Job*>);
+typedef std::chrono::high_resolution_clock::time_point TimeVar;
 
+#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count()
+#define timeNow() std::chrono::high_resolution_clock::now()
 
 /**
  * Generuje dane.
@@ -15,33 +19,36 @@ typedef std::vector<Job*> (*fun)(std::vector<Job*>);
  * @param seed Ziarno losowania.
  * @return Zwraca nieuporządkowane zadania.
  */
-std::vector<Job*> generateOperations(const int &n, const int &seed) {
+std::vector<Job *> generateOperations(const int &n, const int &seed)
+{
     RandomNumberGenerator randf(seed);
-    std::vector<Job*> J;
+    std::vector<Job *> J;
     int A = 0;
 
-    for(int j = 1; j <= n; ++j) {
-        Job* tmp = new Job(j);
+    for (int j = 1; j <= n; ++j)
+    {
+        Job *tmp = new Job(j);
         J.push_back(tmp);
     }
 
-    for(int j = 0; j < n; ++j) {
+    for (int j = 0; j < n; ++j)
+    {
         J[j]->processingTime = randf.nextInt(1, 29);
         A += J[j]->processingTime;
     }
 
-    for(int j = 0; j < n; ++j) {
+    for (int j = 0; j < n; ++j)
+    {
         J[j]->weight = randf.nextInt(1, 9);
-    }    
+    }
 
-    for(int j = 0; j < n; ++j) {
+    for (int j = 0; j < n; ++j)
+    {
         J[j]->deadline = randf.nextInt(1, 29);
     }
 
     return J;
 }
-
-
 
 /**
  * Wypisywanie raportu.
@@ -49,78 +56,98 @@ std::vector<Job*> generateOperations(const int &n, const int &seed) {
  * @param name Nazwa zadania.
  * @param pi Wektor z uporządkowanymi zadaniami.
  */
-void log(const char* name, std::vector<Job*> pi, fun ptr = nullptr) {
-    
+std::chrono::duration<double> log(const char *name, std::vector<Job *> pi, fun ptr = nullptr)
+{
+
     auto start = std::chrono::system_clock::now();
-    if(ptr != nullptr)  pi = ptr(pi);
+    if (ptr != nullptr)
+        pi = ptr(pi);
     auto end = std::chrono::system_clock::now();
 
-    int size = pi.size();
-    int *C = new int[size];
-    int *T = new int[size];
+    // int size = pi.size();
+    // int *C = new int[size];
+    // int *T = new int[size];
 
-    printf("%s\npi: [", name);
-    for(int i = 0; i < size; ++i) {
-        printf("%3d", pi[i]->number);
+    // printf("%s\npi: [", name);
+    // for (int i = 0; i < size; ++i)
+    // {
+    //     printf("%3d", pi[i]->number);
 
-        if(i != size - 1) printf(", ");
-    }
-    printf("]\n");
+    //     if (i != size - 1)
+    //         printf(", ");
+    // }
+    // printf("]\n");
 
+    // printf("C:  [");
+    // for (int i = 0; i < size; ++i)
+    // {
+    //     if (i)
+    //         C[i] = C[i - 1] + pi[i]->processingTime;
+    //     else
+    //         C[i] = pi[i]->processingTime;
 
-    printf("C:  [");
-    for(int i = 0; i < size; ++i) {
-        if(i) C[i] = C[i-1] + pi[i]->processingTime;
-        else C[i] = pi[i]->processingTime;
-        
-        printf("%3d", C[i]);
-        if(i != size - 1) printf(", ");
-    }
-    printf("]\n");
+    //     printf("%3d", C[i]);
+    //     if (i != size - 1)
+    //         printf(", ");
+    // }
+    // printf("]\n");
 
+    // printf("T:  [");
+    // for (int i = 0; i < size; ++i)
+    // {
+    //     T[i] = std::max(C[i] - pi[i]->deadline, 0);
+    //     printf("%3d", T[i]);
+    //     if (i != size - 1)
+    //         printf(", ");
+    // }
+    // printf("]\n");
 
-    printf("T:  [");
-    for(int i = 0; i < size; ++i) {
-        T[i] = std::max(C[i] - pi[i]->deadline, 0);
-        printf("%3d", T[i]);
-        if(i != size - 1) printf(", ");
-    }
-    printf("]\n");
+    // printf("wT: [");
+    // int wiTi = 0;
+    // for (int i = 0; i < size; ++i)
+    // {
+    //     int wT = T[i] * pi[i]->weight;
+    //     wiTi += wT;
+    //     printf("%3d", wT);
+    //     if (i != size - 1)
+    //         printf(", ");
+    // }
+    // printf("]\n");
 
-    printf("wT: [");
-    int wiTi = 0;
-    for(int i = 0; i < size; ++i) {
-        int wT = T[i] * pi[i]->weight;
-        wiTi += wT;
-        printf("%3d", wT);
-        if(i != size - 1) printf(", ");
-    }
-    printf("]\n");
+    std::chrono::duration<double> diff = end - start;
+    // printf("wiTi: %d\n", wiTi);
+    // printf("Time: %fs\n\n", diff);
 
-
-    std::chrono::duration<double> diff = end-start;
-    printf("wiTi: %d\n", wiTi);
-    printf("Time: %fs\n\n", diff);
-
-    delete[] C;
-    delete[] T;
+    // delete[] C;
+    // delete[] T;
+    return diff;
 }
 
-
-int main() {
+void measure_multiple(std::string filename, fun ptr)
+{   
     int seed = 1;
-    int n = 8;
+    std::fstream file;
+    file.open(filename, std::ios::out);
 
-    // std::cin >> seed >> n;
+    for (int i = 5; i < 11; i++)
+    {
+        std::vector<Job *> J = generateOperations(i, seed);
+        std::string s = std::to_string(i) + ";" + std::to_string(duration(log("Greedy", J, ptr))) + ";\n";
+        file << s;
+        // log("Natualna", J);
+        // log("Greedy", J, Greedy);
+        // log("BruteForce", J, BruteForce);
+        // log("Dynamic", J, Dynamic);
+        std::cout << s << std::endl;
+    }
 
-    printf("seed: %d\nrozmiar: %d\n\n", seed, n);
+    file.close();
+}
 
-    std::vector<Job*> J = generateOperations(n, seed);
-
-    log("Naturalna", J);
-    log("Greedy", J, Greedy);
-    log("BruteForce", J, BruteForce);
-    log("Dynamic", J, Dynamic);
-
+int main()
+{
+    // measure_multiple("Greedy.csv", Greedy);
+    measure_multiple("BruteForce.csv", BruteForce);
+    // measure_multiple("Dynamic.csv", Dynamic);
     return 0;
 }
--- benchmark_plot.py
+++ benchmark_plot.py
@@ -8,38 +8,19 @@ time = []
 vals = []
 
 
-with open("Carlier/result.csv", 'r') as file:
+with open("WiTi/BruteForce.csv", 'r') as file:
     reader = csv.reader(file, delimiter=';')
     for row in reader:
         vals.append(int(row[0]))
         time.append(float(row[1]) / 10E6)
 
-vals_lower = []
-vals_higher = []
-
-time_lower = []
-time_higher = []
-
-for (v, t) in zip(vals, time):
-    if v / t < 204:
-        vals_lower.append(v)
-        time_lower.append(t)
-    else:
-        vals_higher.append(v)
-        time_higher.append(t)
 
 plt.figure()
-plt.plot(vals_lower, time_lower, 'bo')
-plt.plot(vals_higher, time_higher, 'ro')
-plt.plot(vals, np.polyval(func_lower := np.polyfit(
-    vals_lower, time_lower, 2), vals), 'c-')
-plt.plot(vals, np.polyval(func_higher := np.polyfit(
-    vals_higher, time_higher, 2), vals), 'g-')
+plt.plot(vals, time, 'bo')
 plt.xlabel(r"Num. of processes")
 plt.ylabel(r"Time (ms)")
+plt.xticks(np.arange(4, 11, 1))
 # create a legend
 plt.grid()
 plt.show()
 
-print(func_lower)
-print(func_higher)
